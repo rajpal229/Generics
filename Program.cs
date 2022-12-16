f6573362a1507565d739c5ff8d152da6dba4ea13
rajpal229 <rajpalrawat229@gmail.com>
2022-12-16 22:41:25 +0530

    Added: UC1_TC1.3

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            int maxint = FindMax.FindIntMax(1, 3, 2);
+            int maxint = FindMax.FindIntMax(1, 2, 3);
             Console.WriteLine("The maximum number is : " + maxint);
         }
     }
