ae734edafe818e685182a5cf7a129aaeed03dfca
rajpal229 <rajpalrawat229@gmail.com>
2022-12-16 22:35:38 +0530

    Added: UC1_TC1.1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxint = FindMax.FindIntMax(3, 2, 1);
+            Console.WriteLine("The maximum number is :" + maxint);
+        }
+    }
+}
